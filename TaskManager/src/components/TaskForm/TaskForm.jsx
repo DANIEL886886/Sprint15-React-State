7d1b56c1c51f4cd3715865c8d413cb00bd0b385a
DANIEL886886 <danielparaschiv886@gmail.com>
2024-07-06 23:00:03 +0300

    project update

--- TaskManager/src/components/TaskForm/TaskForm.jsx
+++ TaskManager/src/components/TaskForm/TaskForm.jsx
@@ -2,26 +2,51 @@ import "./TaskForm.css"
 import React from "react";
 
 function TaskForm() {
+
+    const handleNameChange = (event) => { 
+        console.log(event.target.value)
+    }
+
+    const handleDateChange = (event) => {
+        console.log(event.target.value);
+    }
+
+    const handleDetailsChange = (event) => {
+        console.log(event.target.value);
+    }
+
     return (
 		<div>
 			<form>
 				<div className="form-row">
 					<label>Task Name</label>
-					<input type="text" />
+					<input
+						onChange={handleNameChange}
+						className="input-primary"
+						type="text"
+					/>
 				</div>
 
 				<div className="form-row">
 					<label>Due Date</label>
-					<input type="date" />
+					<input
+						onChange={handleDateChange}
+						className="input-primary"
+						type="date"
+					/>
 				</div>
 
 				<div className="form-row">
 					<label>Task details</label>
-                    <input type="text" />
-                    <textarea name="" id="" cols="30" rows="10"></textarea>
+					<input
+						onChange={handleDetailsChange}
+						className="input-primary"
+						type="text"
+					/>
+					<textarea name="" id="" cols="30" rows="10"></textarea>
 				</div>
 
-                <button type="submit">Create Task</button>
+				<button type="submit">Create Task</button>
 			</form>
 		</div>
 	);
