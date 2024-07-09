3ecc372efb408ca139be6b7e730f0cd7850ba731
DANIEL886886 <danielparaschiv886@gmail.com>
2024-07-09 13:33:28 +0300

    In TaskForm.js replaced tabs spaces with spaces

--- TaskManager/src/components/TaskForm/TaskForm.jsx
+++ TaskManager/src/components/TaskForm/TaskForm.jsx
@@ -2,105 +2,79 @@ import "./TaskForm.css"
 import React, { useState } from "react";
 
 function TaskForm(props) {
-	const initialFormData = {
-		status:"",
-		dueDate: "",
-		details: "",
-	};
+ const initialFormData = {
+  status:"",
+  dueDate: "",
+  details: "",
+ };
 
-	const [formData, setFormData] = useState({
-		status: "",
-		dueDate: "",
-		details: "",
-	});
+ const [formData, setFormData] = useState({
+  status: "",
+  dueDate: "",
+  details: "",
+ });
 
-	const handleInputChange = (event) => {
-		setFormData((prevState) => ({
-			...prevState,
-			[event.target.name]: event.target.value,
-		}));
-	};
+ const handleInputChange = (event) => {
+  setFormData((prevState) => ({
+  ...prevState,
+  [event.target.name]: event.target.value,
+  }));
+ };
 
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		console.log("FormData is : ", formData);
-		//initial state of "formData"
-		setFormData({ ...initialFormData });
+ const handleSubmit = (event) => {
+  event.preventDefault();
+  console.log("FormData is : ", formData)
+  setFormData({ ...initialFormData });
+  props.addNewTask(formData);
+ };
 
-		// bond to create new task
-		props.addNewTask(formData);
-		// ............................
-	};
+ return (
+  <div>
+   <form onSubmit={handleSubmit}>
+   <div className="form-row">
+    <label>Task Status</label>
+    <input
+     value={formData.status}
+     name="status"
+     onChange={handleInputChange}
+     className="input-primary"
+     type="text"
+    />
+  </div>
 
-	// const handleNameChange = (event) => {
-	//     setFormData((prevState) => ({
-	//         ...prevState,
-	//         taskName: event.target.value
-	//         })
-	//     );
-	// }
+  <div className="form-row">
+    <label>Due Date</label>
+    <input
+     value={formData.dueDate}
+     name="dueDate"
+     onChange={handleInputChange}
+     className="input-primary"
+     type="date"
+    />
+  </div>
 
-	// const handleDateChange = (event) => {
-	//     setFormData((prevState) => ({
-	// 		...prevState,
-	// 		dueDate: event.target.value,
-	//     }));
-	// }
+  <div className="form-row">
+    <label>Task details</label>
+    <textarea
+     value={formData.details}
+     name="details"
+     onChange={handleInputChange}
+     className="input-primary"
+     type="text"
+     id=""
+     cols="3"
+     rows="10"
+    />
+  </div>
 
-	// const handleDetailsChange = (event) => {
-	//     setFormData((prevState) => ({
-	//         ...prevState,
-	//         taskDetails: event.target.value,
-	//     }));
-	// }
-
-	return (
-		<div>
-			<form onSubmit={handleSubmit}>
-				<div className="form-row">
-					<label>Task Status</label>
-					<input
-						value={formData.status}
-						name="status"
-						onChange={handleInputChange}
-						className="input-primary"
-						type="text"
-					/>
-				</div>
-
-				<div className="form-row">
-					<label>Due Date</label>
-					<input
-						value={formData.dueDate}
-						name="dueDate"
-						onChange={handleInputChange}
-						className="input-primary"
-						type="date"
-					/>
-				</div>
-
-				<div className="form-row">
-					<label>Task details</label>
-					<textarea
-						value={formData.details}
-						name="details"
-						onChange={handleInputChange}
-						className="input-primary"
-						type="text"
-						id=""
-						cols="3"
-						rows="10"
-					/>
-				</div>
-
-				<div className="form-btn-wrapper">
-					<button className="button-primary" type="submit">
-						Create Task
-					</button>
-				</div>
-			</form>
-		</div>
-	);
+  <div className="form-btn-wrapper">
+    <button className="button-primary" type="submit">
+    Create Task
+    </button>
+  </div>
+ </form>
+</div>
+ );
 }
 
 export default TaskForm;
