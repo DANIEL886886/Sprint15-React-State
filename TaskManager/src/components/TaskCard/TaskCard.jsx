2dc980c708af558e27e5bc983a88520b44617c51
DANIEL886886 <danielparaschiv886@gmail.com>
2024-07-09 13:25:51 +0300

    In TaskCard.js replaced tabs spaces with spaces

--- TaskManager/src/components/TaskCard/TaskCard.jsx
+++ TaskManager/src/components/TaskCard/TaskCard.jsx
@@ -5,45 +5,38 @@ import React, { useState } from "react";
 
 function TaskCard(props) {
 
-  const [taskId, setTaskId] = useState(props.id)
+ const [taskId, setTaskId] = useState(props.id)
 
-  const handleClick = (e) => {
-		e.stopPropagation();
-		props.onDeleteTask(props.id);
-  };
+ const handleClick = (e) => {
+  e.stopPropagation();
+  props.onDeleteTask(props.id);
+ };
 
-  const focusedClass = props.isFocused ? "focused" : "";
+ const focusedClass = props.isFocused ? "focused" : "";
 
-  const focusedBtnClass = props.isFocused ? "focusedbtn" : "";
+ const focusedBtnClass = props.isFocused ? "focusedbtn" : "";
   
-  function setFocused() {
-    props.markAsFocus()
-  }
+ function setFocused() {
+  props.markAsFocus()
+ }
 
-  return (
-		<div className={`card-wrapper ${focusedClass}`} onClick={setFocused}>
-			<div className="card-header">
-				<p className="task-id">{taskId}</p>
-				<Badge status={props.status} />
-			</div>
+ return (
+ <div className={`card-wrapper ${focusedClass}`} onClick={setFocused}>
+   <div className="card-header">
+	  <p className="task-id">{taskId}</p>
+	  <Badge status={props.status} />
+	 </div>
 
-			<div className="card-content">
-				<p>{props.details}</p>
-			</div>
+	 <div className="card-content">
+    <p>{props.details}</p>
+	 </div>
 
-			<div className="card-footer">
-				<DateContainer date={props.dueDate} />
-				{props.isFocused ? (
-          <button
-            className="task-btn" onClick={handleClick}>
-						Delete
-					</button>
-				) : (
-					""
-				)}
-			</div>
-		</div>
-  );
+	 <div className="card-footer">
+    <DateContainer date={props.dueDate} />
+    {props.isFocused ? (<button className="task-btn" onClick={handleClick}>Delete</button>) : ("")}
+	 </div>
+ </div>
+);
 }
 
 export default TaskCard;
